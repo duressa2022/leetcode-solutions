31694a149ff563a5289c2da431fe4bd26da3e428
Duressa <127621345+duressa2022@users.noreply.github.com>
2023-12-20 08:49:17 -0800

    Added solution for Check if Number is a Sum of Powers of Three

---
+++ leet-code-solutions/check-if-number-is-a-sum-of-powers-of-three.py
@@ -0,0 +1,10 @@
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        ternary=""
+        while n>0:
+            ternary=str(n%3)+ternary
+            n=n//3
+        return False if "2" in  ternary else True
+        
+
+        
