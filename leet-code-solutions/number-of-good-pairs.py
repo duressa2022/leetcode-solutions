fe067ace757bd315591bc03849982cc737338ed7
Duressa <127621345+duressa2022@users.noreply.github.com>
2023-12-23 10:43:49 -0800

    Added solution for Number of Good Pairs

--- leet-code-solutions/number-of-good-pairs.py
+++ leet-code-solutions/number-of-good-pairs.py
@@ -1,9 +1,14 @@
-class Solution:
-    def numIdenticalPairs(self, nums: List[int]) -> int:
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         counter=0
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
-                if nums[i]==nums[j]:
+                if i<j and nums[i]==nums[j]:
                     counter+=1
         return counter
+            
         
