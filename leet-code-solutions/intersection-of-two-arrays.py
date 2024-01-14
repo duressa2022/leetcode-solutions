a3a0e48468fa307d977ab93de718a996c25a9b1b
Duressa <127621345+duressa2022@users.noreply.github.com>
2024-01-14 22:09:08 +0300

    Added solution for Intersection of Two Arrays

--- leet-code-solutions/intersection-of-two-arrays.py
+++ leet-code-solutions/intersection-of-two-arrays.py
@@ -1,8 +1,4 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        hash_set=set()
-        for number in nums1:
-            if number in nums2:
-                hash_set.add(number)
-        return list(hash_set)
+        return list(set(nums1).intersection(set(nums2)))
         
