7fee082a73d20f53a661b5bcd0cbe9624ee410f5
Duressa <127621345+duressa2022@users.noreply.github.com>
2024-02-28 23:06:02 -0900

    Add solution for Sum of Root To Leaf Binary Numbers

---
+++ leet-code-solutions/leetcode/sum-of-root-to-leaf-binary-numbers.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+        def helperFunction(node,current):
+            if not node:
+                return 0
+            current=(current<<1)|node.val
+            if not node.left and not node.right:
+                return current
+            left=helperFunction(node.left,current)
+            right=helperFunction(node.right,current)
+            return left+right
+        return helperFunction(root,0)
+
+
+        
