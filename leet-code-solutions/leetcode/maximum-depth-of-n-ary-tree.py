ef32a2fcf5356eff6323b83e1f91df241ef77373
Duressa <127621345+duressa2022@users.noreply.github.com>
2024-02-28 23:52:30 -0900

    Add solution for Maximum Depth of N-ary Tree

---
+++ leet-code-solutions/leetcode/maximum-depth-of-n-ary-tree.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        if not root.children:
+            return 1
+        result=[]
+        for child in root.children:
+            result.append(self.maxDepth(child))
+        return max(result)+1
+
+        
