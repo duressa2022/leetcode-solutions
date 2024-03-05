56d84d9fb9672065c08526318bca18af2ca06cc0
Duressa <127621345+duressa2022@users.noreply.github.com>
2024-03-05 10:43:52 -0900

    Add solution for Permutations II

--- leet-code-solutions/leetcode/permutations-ii.py
+++ leet-code-solutions/leetcode/permutations-ii.py
@@ -1,22 +1,21 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         counter=Counter(nums)
+        tempResult=set()
+        finalResult=[]
         length=len(nums)
-        result=set()
         def helperFunction(path):
-            if len(path)==length:
-                result.add(tuple(path[:]))
+            if length==len(path):
+                tempResult.add(tuple(path[:]))
                 return 
             for num in nums:
                 if counter[num]!=0:
-                    counter[num]-=1
                     path.append(num)
+                    counter[num]-=1
                     helperFunction(path)
                     counter[num]+=1
                     path.pop()
         helperFunction([])
-        final=[]
-        for value in result:
-            final.append(list(value))
-        return final
-        
+        for result in tempResult:
+            finalResult.append(list(result))
+        return finalResult
