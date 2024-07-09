31c9216f9a34dd99e6a9dd3c209a9fc4857a3f91
Duressa <127621345+duressa2022@users.noreply.github.com>
2024-07-09 12:28:12 +0300

    Added solution for Reverse String

---
+++ leet-code-solutions/reverse-string.go
@@ -0,0 +1,9 @@
+func reverseString(s []byte)  {
+    left,right:=0,len(s)-1
+    for left<right{
+        s[left],s[right]=s[right],s[left]
+        left=left+1
+        right=right-1
+    }
+    
+}
